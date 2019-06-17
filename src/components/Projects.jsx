afec25380195a164912bea4d2f8224c7a707e05e
Share Azcárraga <shareni.azcarraga@gmail.com>
2019-06-17 11:26:12 -0500

    Added Firebase
    Changed the way projects are displayed from a static, hard-coded fashion to using firebase to generating a card dynamically for each project. As of now there is an issue in which the image is not being displayed. Should fix that later.

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,8 +1,9 @@
 import React from "react";
+import firebase from "../firebase";
 import Container from "react-bootstrap/Container";
 import styled from "styled-components";
 import Button from "react-bootstrap/Button";
-import LLogo from "../assets/LFavicon.png";
+//import LLogo from "../assets/LFavicon.png";
 
 
 const Styles = styled.div`
@@ -39,15 +40,77 @@ const Styles = styled.div`
 `;
 
 class Projects extends React.Component {
+
+  constructor() {
+    super();
+    this.state = {
+      projects: []
+    }
+  }
+
+  componentDidMount() {
+    const dBRef = firebase.database().ref();
+    const projectsRef = dBRef.child('projects');
+    projectsRef.on('value', snap => {
+      let portProject = snap.val();
+      let newState = [];
+      for (let project in portProject) {
+        newState.push ({
+          id: project,
+          name: portProject[project].name,
+          description: portProject[project].description,
+          img: portProject[project].img
+        })
+      }
+      this.setState({
+        projects: newState
+      })
+    })
+  }
+
+  render() {
+    return (
+      <Container>
+        <Styles>
+      <div className="Projects">
+        <h1>Some of my work here</h1><br />
+        {this.state.projects.map((project) => {
+          return(
+            <div className="projectCard">
+            <div className="content projectImage">
+              <img src={project.img} className="thumbnail" alt="" />
+            </div>
+            <div className="content projectInfo">
+              <h2>{project.name}</h2>
+              <p>{project.description}</p>
+              <Button variant="link">Read More</Button>
+            </div>
+          </div>
+          )
+        })}
+      </div>
+      </Styles>
+      </Container>
+    )
+  }
+
+}
+
+export default Projects;
+
+
+//Static Project divs
+/*
   render() {
     return (
       <Container>
         <h2>Some of my work here</h2> <br />
         <Styles>
 
+          {this.state.projects.map}
           <div className="projectCard">
             <div className="content projectImage">
-              <img src={LLogo} className="thumbnail" />
+              <img src={LLogo} className="thumbnail" alt="Organa" />
             </div>
             <div className="content projectInfo">
               <h2>Organa</h2>
@@ -58,7 +121,7 @@ class Projects extends React.Component {
 
           <div className="projectCard">
             <div className="content projectImage">
-              <img src={LLogo} className="thumbnail" />
+              <img src={LLogo} className="thumbnail" alt="Burger Queen" />
             </div>
             <div className="content projectInfo">
               <h2>Burger Queen</h2>
@@ -69,7 +132,7 @@ class Projects extends React.Component {
 
           <div className="projectCard">
             <div className="content projectImage">
-              <img src={LLogo} className="thumbnail" />
+              <img src={LLogo} className="thumbnail" alt="Pokedex Go" />
             </div>
             <div className="content projectInfo">
               <h2>Pokédex Go!</h2>
@@ -83,5 +146,81 @@ class Projects extends React.Component {
     );
   }
 }
+*/
+
+
+
+//Individual project card
+/*
+<div className="projectCard">
+            <div className="content projectImage">
+              <img src={project.img} className="thumbnail" alt="" />
+            </div>
+            <div className="content projectInfo">
+              <h2>{project.name}</h2>
+              <p>{project.descAription}</p>
+              <Button variant="link">Read More</Button>
+            </div>
+          </div>
+*/
+
+//Unsuccessful attempt with React
+/* 
+class Projects extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      projects: [],
+      speed:10,
+    }
+  }
+
+  componentDidMount () {
+    const dBRef = firebase.database().ref();
+    const projectsRef = dBRef.child('projects');
+    projectsRef.on('value', snap => {
+      let portfolio = snap.val();
+      let newState = [];
+      for (let project in portfolio) {
+        newState.push({
+          id: project,
+          name: portfolio[project].name,
+          description: portfolio[project].description,
+          img: portfolio[project].img
+        })
+      }
+      this.setState({
+        projects: newState
+      })
+    })
+  }
+
+  render() {
+    return (
+      <Container>
+        <h2>Some of my work here</h2> <br />
+        <Styles>
+
+          {this.state.projects.map((project) => {
+            return (
+
+            <div>
+              <h3>{project.name}</h3>
+              <p>{project.description}</p>
+              <p>{project.img}</p>
+            </div>
+          
+
+            );
+          })}
+          
+
+        </Styles>
+      </Container>
+    );
+  }
+}
 
 export default Projects;
+*/
