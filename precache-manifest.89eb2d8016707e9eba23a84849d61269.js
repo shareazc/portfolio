e6b0ad211f3ba7feef5f9e23178ad68b5204f5b0
Share Azcárraga <shareni.azcarraga@gmail.com>
2019-07-15 21:26:03 -0500

    Updates

---
+++ precache-manifest.89eb2d8016707e9eba23a84849d61269.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "71e348e3e0073c726d3cdfda202ba4b2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "485bd57d2ee5fc6cb1d9",
+    "url": "/static/css/main.20964fa2.chunk.css"
+  },
+  {
+    "revision": "669f22340621fd6a52a7",
+    "url": "/static/js/2.f28e1b2d.chunk.js"
+  },
+  {
+    "revision": "485bd57d2ee5fc6cb1d9",
+    "url": "/static/js/main.c6197d42.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "1348113a7b060567c3259c34728f9d37",
+    "url": "/static/media/CV-Share-Azcarraga.1348113a.pdf"
+  },
+  {
+    "revision": "a7a97480d5a4b214ea90071b853337cf",
+    "url": "/static/media/DnD-Cropped.a7a97480.jpg"
+  },
+  {
+    "revision": "49c6c26a91160b74c0d8393170596063",
+    "url": "/static/media/mockupOrgana.49c6c26a.jpg"
+  },
+  {
+    "revision": "76c04bafe42297da17d24774d6812782",
+    "url": "/static/media/mockupOrganaMob.76c04baf.png"
+  },
+  {
+    "revision": "1f51250860d98fa7a94b8ea07318331a",
+    "url": "/static/media/self.1f512508.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.79dba668ee4e90f3060227d1c1433cf1.js"
+  "/precache-manifest.89eb2d8016707e9eba23a84849d61269.js"
 );
 
 self.addEventListener('message', (event) => {
